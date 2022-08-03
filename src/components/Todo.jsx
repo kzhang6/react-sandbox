a73cd5e9316e01e0200808025ee667d0b2d53903
Kristine Zhang <kristine.zhang@cognosante.com>
2022-08-03 14:01:21 -0700

    useRef fixes "can't perform state update on an unmounted component" (which isn't a problem with react18 anymore)?

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,6 +5,8 @@ function Todo() {
     const [loading, setLoading] = useState(true)
     const [todo, setTodo] = useState({})
 
+    const isMounted = useRef(true)
+
     useEffect(() => {
         fetch('http://jsonplaceholder.typicode.com/todos/1')
         .then((res) => res.json())
@@ -14,6 +16,11 @@ function Todo() {
                 setLoading(false)
             }, 3000)
         }))
+
+        //runs when component is unmounted
+        return () => {
+            console.log(123)
+        }
     }, [])
   
     return loading? <h3>Loading...</h3> : <h1>{todo.title}</h1>
