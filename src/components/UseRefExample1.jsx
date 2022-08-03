7537073f3c3b97660b18a1000b3d320a74f87b3b
Kristine Zhang <kristine.zhang@cognosante.com>
2022-08-03 10:22:56 -0700

    First useRef

--- src/components/UseRefExample1.jsx
+++ src/components/UseRefExample1.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 
+import {useRef} from 'react'
+
 function UseRefExample1() {
-  return (
-    <div>
-        <form>
-            <label htmlFor="name">Name</label>
-            <input 
-                type='text'
-                id='name'
-                className='form-control mb-2'
-            />
-            <button type="submit" className='btn btn-primary'>Submit</button>
-        </form>
-    </div>
-  )
+    const inputRef = useRef()
+
+    return (
+        <div>
+            <form>
+                <label htmlFor="name">Name</label>
+                <input 
+                    type='text'
+                    ref={inputRef}  //reference to this input
+                    id='name'
+                    className='form-control mb-2'
+                />
+                <button type="submit" className='btn btn-primary'>Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default UseRefExample1
