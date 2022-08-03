9abe2af1ee9c453cbac96329bfd97ebfd85f45f2
Kristine Zhang <kristine.zhang@cognosante.com>
2022-08-03 11:32:45 -0700

    useRef used on <p>

--- src/components/UseRefExample1.jsx
+++ src/components/UseRefExample1.jsx
@@ -4,12 +4,14 @@ import {useRef} from 'react'
 
 function UseRefExample1() {
     const inputRef = useRef()
+    const paraRef = useRef()
 
     const onSubmit = (e) => {
         e.preventDefault()
         console.log(inputRef.current.value)             //inputRef is a DOM element
         inputRef.current.value = 'Hello'                //set value of the input
         inputRef.current.style.backgroundColor = 'red'
+        paraRef.current.innerText = 'Goodbye'           //paraRef changes text when clikcing onSubmit
     }
 
     return (
@@ -23,6 +25,7 @@ function UseRefExample1() {
                     className='form-control mb-2'
                 />
                 <button type="submit" className='btn btn-primary'>Submit</button>
+                <p ref={paraRef}>Helllo</p>
             </form>
         </div>
     )
